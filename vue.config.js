e37c898e451f78837fd5c73b2f02b3c48b183522
vasingwork <vasingwork@gmail.com>
2023-03-14 12:12:07 +0800

    first time commit

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+function resolve (dir) {
+    return path.join(__dirname, './', dir)
+  }
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: false,
+  chainWebpack(config){
+    config.module
+    .rule('svg')
+    .exclude.add(resolve('src/assets/icons'))
+    .end()
+    config.module
+    .rule('icons')
+    .test(/\.svg$/)
+    .include.add(resolve('src/assets/icons'))
+    .end().use('svg-sprite-loader')
+    .loader('svg-sprite-loader')
+    .options({
+      symbolId: 'icon-[name]'
+    }).end()
+  }
+})
+
+
+ 
